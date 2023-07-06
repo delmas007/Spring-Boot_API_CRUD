7ba7602bd773abdab3e1b54bb89ccdbc1c486064
delmas <azertyazerty1ze@gmail.com>
2023-07-06 00:31:51 +0000

    premier commit

---
+++ src/main/java/com/delmasapi/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.delmasapi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delmasapi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delmasapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
